b9d4a23540d9557c8edfeed482e9a23b9bc3516c
Suciu Andrei Cornel <horizont8@yahoo.com>
2022-02-28 13:33:11 +0200

    Before Native

--- assets/js/components/Index/NavbarIndex.js
+++ assets/js/components/Index/NavbarIndex.js
@@ -1,79 +1,67 @@
-import Link from 'next/link'
 import React from "react";
 import {useUser} from "@auth0/nextjs-auth0";
 
 
-export default function NavbarComponent(props) {
+export default function NavbarComponent() {
     const { user, isLoading } = useUser();
-    console.log(user)
-    if(user) {
         return (
-            <div className="nav_buttons">
-                <div>
-                    <a id="tag-Username"
-                       className="logged_in_as"
-                       style={{
-                           color: 'whitesmoke',
-                           fontSize: 15,
-                       }}
-                    >Logged in as {user[0].email}
-                    </a>
-                    {" "}
-                    <Link href="api/auth/logout"
-                          id="button-LogouT">
-                        <button
-                            className="btn btn-primary"
-                            style={{
-                                color: 'whitesmoke',
-                             fontSize: 15,
-                             }}
-                        >Log Out
-                        </button>
-                    </Link>
-                </div>
-            </div>
+            <>
+                {user && (
+                    <div className="nav_buttons">
+                        <div>
+                            <a id="tag-Username"
+                               className="logged_in_as"
+                               style={{
+                                   color: 'whitesmoke',
+                                   fontSize: 15,
+                               }}
+                            >Logged in as {user.nickname}
+                            </a>
+                            {" "}
+                            <a    href={`api/auth/logout`}
+                                  id="button-Logout">
+                                <button
+                                    className="btn btn-primary"
+                                    style={{
+                                        color: 'whitesmoke',
+                                        fontSize: 15,
+                                    }}
+                                >Log Out
+                                </button>
+                            </a>
+                        </div>
+                    </div>
+                )}
+                {!user && !isLoading &&(
+                    <div className="nav_buttons">
+                        <div>
+                            <a id="tag-not-logged-in"
+                               className="not_logged_in"
+                               style={{
+                                   color: 'whitesmoke',
+                                   fontSize: 15,
+                               }}
+                            >Not logged in
+                            </a>
+                            {" "}
+                            <a    href={`api/auth/login`}
+                                  id="button-Login"
+                            >
+                                <button
+                                    className="btn btn-primary"
+                                    style={{
+                                        color: 'whitesmoke',
+                                        fontSize: 15,
+                                    }}
+                                >Log In
+                                </button>
+                            </a>
+                            {" "}
+                        </div>
+                    </div>
+                )}
+            </>
         )
-    } else {
-        return (
-            <div className="nav_buttons">
-                <div>
-                    <a id="tag-not-logged-in"
-                       className="not_logged_in"
-                       style={{
-                           color: 'whitesmoke',
-                           fontSize: 15,
-                       }}
-                    >Not logged in
-                    </a>
-                    {" "}
-                    <Link href="api/auth/login"
-                          id="button-Login"
-                          >
-                        <button
-                            className="btn btn-primary"
-                            style={{
-                                color: 'whitesmoke',
-                                fontSize: 15,
-                            }}
-                        >Log In
-                        </button>
-                    </Link>
-                    {" "}
-                    <Link href="/register"
-                          id="button-Signup">
-                        <button
-                            className={"btn btn-dark"}
-                            style={{
-                                color: 'whitesmoke',
-                                fontSize: 15,
-                            }}
-                        >Sign up
-                        </button>
-                    </Link>
-                </div>
-            </div>
-        )
-    }
 }
 NavbarComponent.propTypes = {
 }
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,30 @@
-//import React from "react";
-
 import FilterTodo from "../assets/js/components/Index/newIndexFilter";
 import Header from "../assets/js/components/Index/Header";
 import Footer from "../assets/js/components/Index/Footer";
 import React from "react";
+import {useUser} from "@auth0/nextjs-auth0";
 
-//import Head from 'next/head'
+export default function Home() {
+    const { user, isLoading } = useUser();
+
+    return (
+            <>
+                <Header/>
+                {user && (
+                    <FilterTodo/>
+                )}
+                {!user && !isLoading &&(
+                    <>
+                    <customTitle>
+                        Loading...
+                    </customTitle>
+                    </>
+                )}
+                <Footer/>
+
+            </>
+        )
+}
 
 
-const Home = () => (
-  <>
-      <Header/>
-      <FilterTodo/>
-      <Footer/>
-  </>
-)
 
-export default Home;
